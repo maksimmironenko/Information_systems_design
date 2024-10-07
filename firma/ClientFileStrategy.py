cb72377c25a14ca39ef42951f4075fb743c4d2f2
MrSergey98 <112868520+MrSergey98@users.noreply.github.com>
2024-10-07 16:36:59 +0300

    Add files via upload

---
+++ firma/ClientFileStrategy.py
@@ -0,0 +1,11 @@
+from builtins import NotImplementedError
+
+from firma.ClientRepFile import ClientRepFile
+
+class ClientFileStrategy(ClientRepFile):
+
+    def get_data_from_file_specific(self):
+        raise NotImplementedError('This method shod be implemented.')
+
+    def write_data_to_file(self, data):
+        raise NotImplementedError('This method shod be implemented.')
