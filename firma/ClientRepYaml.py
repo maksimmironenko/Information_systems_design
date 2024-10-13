05ce4f71aff508d8864c33987e49c0acb141a298
MrSergey <mrsergey98@mail.ru>
2024-10-13 21:02:33 +0300

    fix(LR2): ClientRepYaml according ro file strategy

--- firma/ClientRepYaml.py
+++ firma/ClientRepYaml.py
@@ -1,20 +1,20 @@
 import yaml
 
 from root.settings import BASE_DIR
-from firma.ClientRepFile import ClientRepFile
+from firma.ClientFileStrategy import ClientFileStrategy
 
 
-class ClientRepYaml(ClientRepFile):
+class ClientRepYaml(ClientFileStrategy):
 
     @classmethod
-    def _get_data_from_file_specific(cls):
+    def get_data_from_file_specific(cls):
         path_to_file = BASE_DIR / 'db_yaml.yaml'
         with open(path_to_file) as file:
             data = yaml.safe_load(file)
         return data if data else []
 
     @classmethod
-    def _write_data_to_file(cls, data):
+    def write_data_to_file(cls, data):
         path_to_file = BASE_DIR / 'db_yaml.yaml'
         with open(path_to_file, mode='w') as file:
             yaml.dump(data, file)
